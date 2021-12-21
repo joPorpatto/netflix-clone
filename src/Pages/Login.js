ff5e1e20783b17e12d0bf5e191ca43da5da44b5a
joPorpatto <joel.porpatto@gmail.com>
2021-12-21 20:03:44 -0300

    fixed nav

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -55,7 +55,7 @@ export const Login = () => {
                                                                                 </form>
                                                                         </div>
 
-                                        }   
+                                     }   
                                 </div>
                         </div>
 
